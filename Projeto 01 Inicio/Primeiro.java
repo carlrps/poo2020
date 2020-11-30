2cdcb14fff71d4d4c001fd786f1322acff236e06
carlrps <carlosryan24@gmail.com>
2020-11-29 23:20:55 -0300

    Update Primeiro.java

--- Projeto 01 Inicio/Primeiro.java
+++ Projeto 01 Inicio/Primeiro.java
@@ -1,10 +1,10 @@
 class divisao	{
     public	static	void	main(String[]	args) {
     int x = 20;
-    int y = 100;
+    int y = 500;
     for	(int i = x;	i <	y; i++)	{
-    if (i % 19 == 0)	{
-        System.out.println("Achei um número	divisível por 19 entre x e y");
+    if (i % 50 == 0)	{
+        System.out.println("Achei um número	divisível por 50 entre x e y");
         break;
     }
 }
