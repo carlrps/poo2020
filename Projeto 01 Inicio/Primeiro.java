d63f3671d24c3756b6b6fdc35e435305e0385863
carlrps <carlosryan24@gmail.com>
2020-11-29 23:17:19 -0300

    Create Primeiro.java

---
+++ Projeto 01 Inicio/Primeiro.java
@@ -0,0 +1,12 @@
+class divisao	{
+    public	static	void	main(String[]	args) {
+    int x = 20;
+    int y = 100;
+    for	(int i = x;	i <	y; i++)	{
+    if (i % 19 == 0)	{
+        System.out.println("Achei um número	divisível por 19 entre x e y");
+        break;
+    }
+}
+}
+}
